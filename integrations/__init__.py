82f410ba88fce5a4aca78320302b0deb791033be
PDGadm <pdgadmgamer@outlook.es>
2026-01-06 03:46:06 +0100

    Document user-made integration process in __init__.py
    Add instructions for user-made integrations in PyMusic.

---
+++ integrations/__init__.py
@@ -0,0 +1,10 @@
+# PyMusic integrations [TRYING TO MAKE IT WORK FOR USER MADE INTEGRATIONS]
+# Put here your integrations for PyMusic
+
+# Official integrations are: 
+#- streamlabs integration
+
+#how to add?
+# 1 unzip the integration zip file and put it on a folder
+# 2 put that folder inside of this folder
+# 3 start PyMusic and it will detect the integration automatically
